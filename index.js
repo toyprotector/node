5ef11f116c14b6f83233dbd576da37c15e71cee3
toyprotector <63397258+toyprotector@users.noreply.github.com>
2020-04-12 18:29:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ port = process.argv[2] || process.env.PORT || "8080";
 
 server = http.createServer(function (req, res) { res.write("hello world"); res.end(); });
 
-server.listen(port, "127.0.0.1", function () { console.log("running!"); });
+server.listen(port);
 
 
